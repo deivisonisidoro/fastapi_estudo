2d0b56104a8addbe18180021ca958f2540bb27b1
Deivison Isidoro da Silva <61429017+deivisonisidoro@users.noreply.github.com>
2022-10-24 08:12:26 -0300

    fix: adicinando arquivo init nos arquivos

--- src/infra/sqlalchemy/config/database.py
+++ src/infra/sqlalchemy/config/database.py
@@ -4,20 +4,23 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///db.sqlite3")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///db.sqlite3")
+# TEST_DATABASE = os.getenv("DATABASE_URL", "false") in ("true", "yes")
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
 
-def create_db():
+def create_db() -> None:
+    engine = create_engine(DATABASE_URL)
+    Base.metadata.drop_all(engine)
     Base.metadata.create_all(bind=engine)
 
 
-def get_db():
+def get_db() -> None:
     db = SessionLocal()
     try:
         yield db
--- src/server.py
+++ src/server.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.responses import RedirectResponse
 
 from src.routers import router
 
 app = FastAPI()
 
-app.include_router(router, prefix="")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
+
+
+@app.get("/", tags=["Doc Redirect"])
+def redirect():
+    return RedirectResponse(url="/docs/")
+
+
+app.include_router(router, prefix="/api")
